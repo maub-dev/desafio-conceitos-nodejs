61c72fb01cea9018ea3c6a7bb75fe6b6e75d5a67
Mauricio Boff <mau.boff@gmail.com>
2020-04-11 00:20:29 -0300

    Funcionality implemented

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,60 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId (request, response, next) {
+  const { id } = request.params;
+
+  request.app.locals.repositoryIndex = repositories.findIndex(rep => rep.id === id);
+  if (request.app.locals.repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' });
+  }
+
+  return next();
+}
+
+app.use("/repositories/:id", validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = repositories[request.app.locals.repositoryIndex];
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  repositories.splice(request.app.locals.repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",  (request, response) => {
+  const repository = repositories[request.app.locals.repositoryIndex];
+  repository.likes++;
+
+  return response.json(repository);
 });
 
 module.exports = app;
